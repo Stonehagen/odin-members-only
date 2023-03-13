2a2c559eb8ebe747b9c801aa11b598c09abd2829
Stonehagen <steinhagen@mailbox.org>
2023-03-13 14:00:19 +0100

    add sign-up route

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Clubhouse' });
 });
 
+router.get('/sign-up', (req, res) => {
+  res.render('signUp');
+});
+
 module.exports = router;
