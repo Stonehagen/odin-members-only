01edbc62731d017569fec67e35dfd4fc66031a7c
Stonehagen <steinhagen@mailbox.org>
2023-03-15 15:58:37 +0100

    add message write function

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,14 @@ const passport = require('passport');
 
 const router = express.Router();
 const User = require('../models/user');
+const Post = require('../models/post');
+
+const checkAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+};
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -61,5 +69,31 @@ router.post(
 router.get('/log-out', (req, res, next) => {
   req.logout((err) => (err ? next(err) : res.redirect('/')));
 });
+
+router.post('/newMessage', checkAuthenticated, [
+  check('message')
+    .isLength({ min: 10 })
+    .withMessage('Minimum 10 charcters required!')
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('/', { errors: errors.array(), message: req.body.message });
+      return;
+    }
+    // find User
+    const title = `${req.body.message.substring(0, 20)}...`;
+    const author = `${req.user.firstName} + ${req.user.lastName}`;
+    const post = new Post({
+      title,
+      text: req.body.message,
+      author,
+    });
+    post
+      .save()
+      .then(() => res.redirect('/'))
+      .catch((err) => next(err));
+  },
+]);
 
 module.exports = router;
